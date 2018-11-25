59bfe49fa4063892938ad21b9356f5300f3bc0a6
jose <josew383@gmail.com>
2018-11-25 22:25:51 +0100

    Initial commit

---
+++ app/src/main/java/gi1819/trabajo/todorecambios/MainActivity.java
@@ -0,0 +1,13 @@
+package gi1819.trabajo.todorecambios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
